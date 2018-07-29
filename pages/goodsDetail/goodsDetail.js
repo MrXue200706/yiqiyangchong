39b3a5489eede9eb2453f5fe5f44fbb35b1caa72
fawn97 <945814622@qq.com>
2018-07-29 23:57:22 +0800

    传参区分

--- pages/goodsDetail/goodsDetail.js
+++ pages/goodsDetail/goodsDetail.js
@@ -7,7 +7,7 @@ Page({
     is_iframe:false,//点击购买按钮弹出选择弹出层
     type_two:false,//规格2显示
     type_active:false,//修改点击后的样式
-    shoping:false,//是否抢购，否则正常
+    shopping:'normal',//是否抢购，否则正常
   },
   iframeFn(){
     this.setData({is_iframe:!this.data.is_iframe});
@@ -17,6 +17,8 @@ Page({
     this.setData({type_active:!this.data.type_active});
   },
   onLoad(o){
-    console.log(o)
+    if(o.type == 'shopping'){
+      this.setData({shopping:'shopping'})
+    }
   }
 })
