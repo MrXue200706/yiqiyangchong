c6ed93d7da753136ba0a6c2834e2057ff5ebe4e1
fawn97 <945814622@qq.com>
2018-07-29 22:59:10 +0800

    bindtap

--- pages/goodsDetail/goodsDetail.js
+++ pages/goodsDetail/goodsDetail.js
@@ -4,6 +4,15 @@ const app = getApp()
 
 Page({
   data: {
-    
+    is_iframe:false,//点击购买按钮弹出选择弹出层
+    type_two:false,//规格2显示
+    type_active:false,//修改点击后的样式
+  },
+  iframeFn(){
+    this.setData({is_iframe:!this.data.is_iframe});
+  },
+  typeTwoFn(e){
+    this.setData({type_two:true});
+    this.setData({type_active:true});
   }
 })
