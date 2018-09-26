52ee3c19b5a153c5d5852dba527482b5f384fd4f
MrXue200706 <945814622@qq.com>
2018-09-26 11:45:22 +0800

    订单列表切换

--- pages/orderList/orderList.js
+++ pages/orderList/orderList.js
@@ -4,9 +4,17 @@ const app = getApp()
 
 Page({
   data: {
-    orderList: null
+    tab_list:["全部","待付款","待成团","待收货","已完成"],
+    tab_selected_id:0,
   },
   onLoad(options){
 	  
   },
+  tab_select_fn(event){
+    let id = event.currentTarget.id;
+    this.setData({
+      tab_selected_id: id
+    });
+    console.log(11)
+  }
 })
