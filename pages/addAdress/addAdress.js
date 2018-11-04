96cf1d4937688c441354622a4a29978694f3c6c1
luochangtong <44726376+luochangtong@users.noreply.github.com>
2018-11-04 18:34:56 +0800

    Add files via upload

--- pages/addAdress/addAdress.js
+++ pages/addAdress/addAdress.js
@@ -5,13 +5,14 @@ const app = getApp()
 Page({
   data: {
 	adrId: null, //地址ID
+  region: ["广东省","广州市",""],//默认省份地区
 	type: null, //编辑/新增状态
     consignee:null, //收货人
     phoneNumber: null, //手机号码
     address:null ,//收货地址
 		defaultFlag:0,//设为默认的状态
   },
-  onLoad(options){
+  onLoad(options) {
 	if(options.adrId != undefined && options.adrId!= null && options.adrId!="") {
 		//编辑状态，填充数据
 		let that = this;
@@ -41,7 +42,8 @@ Page({
 		wx.setNavigationBarTitle({ title: '编辑收货地址' })  
 	}
   },
-	setDefault(){
+  setDefault() {
+    console.log(!this.data.defaultFlag)
 		this.setData({
 			defaultFlag:!this.data.defaultFlag
 		})
@@ -50,15 +52,26 @@ Page({
     this.data.consignee = e.detail.value;
   },
   getNumber: function (e) {//手机号码
-    this.data.phoneNumber = e.detail.value;
+    let tel = e.detail.value.replace(/[^\d]/g, "")
+    this.setData({
+      phoneNumber:tel
+    })
   },
   getAddress: function (e) {//地址
-    this.data.address = e.detail.value;
+    this.setData({
+      address: e.detail.value
+    })
+  },
+  bindAreaChange: function (e) {//地区修改
+    this.setData({
+      region: e.detail.value
+    })
   },
   //保存收货地址
   saveForm: function(){
+    let area = this.data.region[0] + this.data.region[1] + this.data.region[2];
 	if(this.data.type != null && this.data.type != "" && this.data.type == "editAdr") {
-		//编辑状态
+    //编辑状态
 		wx.request({
 		  url: 'https://wechatapi.vipcsg.com/index/member/update_address',
 		  method: 'POST',
@@ -68,9 +81,9 @@ Page({
 			"is_default": this.data.defaultFlag,
 			"name": this.data.consignee,
 			"phone": this.data.phoneNumber,
-			"addres": this.data.address
+      "addres": this.data.address
 		  }, success(res) {
-			// debugger;
+			console.log(res)
 			if (res.data.result == 1){
 			  //保存成功
 			  wx.navigateTo({
@@ -92,7 +105,7 @@ Page({
 			"is_default": 1,
 			"name": this.data.consignee,
 			"phone": this.data.phoneNumber,
-			"addres": this.data.address
+			"addres": area + this.data.address
 		  }, success(res) {
 			if (res.data.result == 1){
 			  //保存成功
