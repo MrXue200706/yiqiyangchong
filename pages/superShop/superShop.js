6a7f8e68c2e9a20c00a182a11694a40634688fc5
zhengtingb <994012373@qq.com>
2018-12-13 00:04:05 +0800

    分享标题

--- pages/superShop/superShop.js
+++ pages/superShop/superShop.js
@@ -14,15 +14,37 @@ Page({
     })
     this.getSwiper(o.id);
   },
+
+  //请求
+  getSwiper(id) {
+    let that = this;
+    wx.request({
+      url: 'https://wechatapi.vipcsg.com/index/events/goods',
+      data: {
+        events_id:id
+      },
+      success(res) {
+        console.log(res)
+        that.setData({
+          superShop: res.data.data,
+          shareTitle:res.data.data.events_info.events_title 
+        });
+        console.log(that.data.shareTitle)
+        wx.setNavigationBarTitle({
+          title: res.data.data.events_info.events_title 
+        })
+      },
+    })
+  },
   onShareAppMessage: function (options) {
     let that = this
     let urlStr = '/pages/superShop/superShop?id=' + this.data.events_id + '&share_id=' + app.globalData.userInfo.data.data.user_id + '&share_type=goods'
 
     //设置菜单中的转发按钮触发转发事件时的转发内容
     var shareObj = {
-      title: this.data.shareTitle, //转发标题
+      title: "有福利！"+that.data.shareTitle+"发现好东西", //转发标题
       path: urlStr,
-      imgUrl: this.data.swiper[0].img, //图片路径
+     // imgUrl:that.superShop.events_info.events_content_img, //图片路径
       success: function (res) {
         // 分享成功之后的回调
         if (res.errMsg == 'shareAppMessage:ok') {
@@ -82,25 +104,5 @@ Page({
     };
     // 返回shareObj
     return shareObj;
-  },
-  //请求
-  getSwiper(id) {
-    let that = this;
-    wx.request({
-      url: 'https://wechatapi.vipcsg.com/index/events/goods',
-      data: {
-        events_id:id
-      },
-      success(res) {
-        console.log(res)
-        that.setData({
-          superShop: res.data.data,
-          shareTitle:res.data.data.events_info.events_title 
-        });
-        wx.setNavigationBarTitle({
-          title: res.data.data.events_info.events_title 
-        })
-      },
-    })
   },
 })
